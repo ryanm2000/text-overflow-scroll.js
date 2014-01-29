c8085c2aae9c6fc909775da2e777e1bfa77720e0
Ryan Margheriti <ryanmargheriti@gmail.com>
2014-01-29 18:52:54 +1100

    Trying out grunt-gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,16 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    githubPages: {
-      target: {
-        options: {
-          // The default commit message for the gh-pages branch
-          commitMessage: 'push'
-        },
-        // The folder where your gh-pages repo is
-        src: '_site'
-      }
+
+    'gh-pages': {
+      options: {
+        base: 'dist'
+      },
+      src: ['**']
     }
   });
 
-  grunt.loadNpmTasks('grunt-github-pages');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
   grunt.registerTask('deploy', ['githubPages:target']);
 
